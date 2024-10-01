ac0536fa6df246af9a1a1568b740f3157f0b9f89
Nikhil Verma <vermanick75@gmail.com>
2024-10-01 18:53:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.cta-button').addEventListener('click', function() {
+    window.location.href = '#about';
+});
+
+document.querySelector('.quiz-button').addEventListener('click', function() {
+    alert('Quiz functionality will be added soon!');
+});
